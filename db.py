078619a747e2505ae2c335844f0052713dc70545
shadowlegend551 <oskari.j.lappalainen@gmail.com>
2025-11-03 21:31:10 +0200

    Added method for making SQL queries to class DB.

--- db.py
+++ db.py
@@ -8,6 +8,7 @@ class DB:
         if not os.path.isfile(db_file):
             self.__init_db()
 
+
     def __init_db(self) -> None:
         # Create tables for the new database.
         try:
@@ -16,5 +17,18 @@ class DB:
                 pass
 
         except sqlite3.Error:
-            print('DB.__init_db: \
-                   An error occurred while initializing a database.')
+            print('DB.__init_db: '
+                  'An error occurred while initializing a database.')
+
+
+    def execute(self, query: str, args: list[str]=[]) -> list[tuple] | None:
+        try:
+            with sqlite3.connect(self.__db_file) as conn:
+                result = conn.execute(query, args).fetchall()
+        except sqlite3.OperationalError:
+            print('DB.execute: '
+                 f'Couldn\'t execute query "{query}" with arguments {args}')
+            result = None
+            
+        return result
+        
