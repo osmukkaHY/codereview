4dbf0cb84db568aa99b9e071f7843e543f2aea97
shadowlegend551 <oskari.j.lappalainen@gmail.com>
2025-11-04 10:50:14 +0200

    changed app.py to use the config file

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template
 
+from config import db_file
 from db import DB
 
 app = Flask(__name__)
-db = DB('database.db')
+db = DB(db_file)
 
 @app.route('/')
 def index():
