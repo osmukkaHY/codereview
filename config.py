c2a13985baf4d3fdcd2c6d25dd248b5e6ff4e0c2
shadowlegend551 <oskari.j.lappalainen@gmail.com>
2025-11-04 10:48:50 +0200

    Added configureations for db.py and app.py

---
+++ config.py
@@ -0,0 +1,15 @@
+from os import path
+from secrets import token_hex
+
+
+db_schema_file  = 'schema.sql'
+db_file         = 'database.db'
+
+secret          = ''
+if path.isfile('secret.txt'):
+    with open('secret.txt', 'r') as file:
+        secret = file.read()
+else:
+    secret = token_hex(16)
+    with open('secret.txt', 'w') as file:
+        file.write(secret)
