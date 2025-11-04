6ba93e5772f8508729ff5263f79afb4d2b8d1454
shadowlegend551 <oskari.j.lappalainen@gmail.com>
2025-11-04 14:47:48 +0200

    Added login handling logic

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session, flash
+from werkzeug.security import check_password_hash
 
-from config import db_file
+from config import db_file, secret
 from db import DB
 
 app = Flask(__name__)
+app.secret_key = secret
+
 db = DB(db_file)
 
 @app.route('/')
@@ -13,3 +16,21 @@ def index():
 @app.route('/login-form')
 def login_form():
     return render_template('login-form.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+    username = request.form['username']
+    password = request.form['password']
+
+    password_hash = db.execute("""SELECT password_hash
+                                  FROM Users
+                                  WHERE username = ?""", [username])
+    if not password_hash or not check_password_hash(password_hash, password):
+        flash('Incorrect username or password.')
+    else:
+        flash('Login successful!')
+        session['username'] = username
+    
+    return render_template('login-form.html')
+
+
