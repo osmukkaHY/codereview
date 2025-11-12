7266f391c5d9adbc4f3f823493a7a3a626a44850
shadowlegend551 <oskari.j.lappalainen@gmail.com>
2025-11-12 20:22:39 +0200

    Added tables for posts and reviews

--- schema.sql
+++ schema.sql
@@ -3,3 +3,19 @@ CREATE TABLE Users (
     username TEXT UNIQUE NOT NULL,
     password_hash TEXT NOT NULL
 );
+
+CREATE TABLE Posts (
+    id INTEGER PRIMARY KEY,
+    ts DEFAULT TIMESTAMP
+    poster_id INTEGER REFERENCES Users,
+    title TEXT NOT NULL,
+    content TEXT NOT NULL
+);
+
+CREATE TABLE Reviews (
+    id INTEGER PRIMARY KEY,
+    ts DEFAULT TIMESTAMP,
+    reviewer_id INTEGER REFERENCES Users,
+    post_id INTEGER REFERENCES Posts,
+    rating UNSIGNED TINYINT,
+)
