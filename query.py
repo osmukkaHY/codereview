13d58353e06f59173dca3529588edf0550e3620b
osmukkaHY <oskari.lappalainen.hy@gmail.com>
2025-11-17 13:31:57 +0200

    Made Query a dataclass and a factory function

--- query.py
+++ query.py
@@ -1,5 +1,13 @@
+from dataclasses import dataclass
 import sqlite3
 
 
+@dataclass
 class Query:
-    pass
+    _conn:         sqlite3.Connection
+    _query_list:   list[str]
+
+
+
+def query(conn: sqlite3.Connection) -> Query:
+    return Query(conn, [])
