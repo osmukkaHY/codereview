6a76236fdb71a9f258669c9dc784b9c87d4a45bd
osmukkaHY <oskari.lappalainen.hy@gmail.com>
2025-11-17 16:28:42 +0200

    Started making a binds_to argument for sql_clause

--- query.py
+++ query.py
@@ -16,32 +16,42 @@ class Query:
     _conn:         sqlite3.Connection
     _query_list:   list[str]
     _query_type:   QueryType   = None
+    _last_keyword: str         = ''
     _error_status: bool        = False
 
     @staticmethod
-    def sql_clause(keyword: str) -> Callable[['Query', str], 'Query']:
+    def sql_clause(keyword: str, binds_to: list[str]=None) -> Callable[['Query', str], 'Query']:
+        binds_to = [] if not binds_to else binds_to
         def outer_wrapper (func: Callable[["Query", str], str]) -> Callable[["Query", str], "Query"]:
             def append_query(query: "Query", argument: str) -> "Query":
-                if not isinstance(argument, str):
+                # If the keyword has to be first in the query but it isn't. 
+                if not len(binds_to) and query._query_type:
                     query._error_status = True
 
+                # If the last keyword is a wrong one.
+                elif query._last_keyword not in binds_to:
+                    query._error_status = True
+
+                # If the given argument is not a string.
+                elif not isinstance(argument, str):
+                    query._error_status = True
                 if not query._error_status:
                     query._query_list.append(keyword)
                     query._query_list.append(argument)
                 return func(query, argument)
             return append_query
         return outer_wrapper
-        
-
+    
+    
     @sql_clause('SELECT')
     def select(self, argument: str) -> 'Query':
         return self
 
-    @sql_clause('FROM')
+    @sql_clause('FROM', binds_to=['SELECT'])
     def from_(self, argument: str) -> 'Query':
         return self
 
-    @sql_clause('WHERE')
+    @sql_clause('WHERE', binds_to=['FROM'])
     def where(self, argument: str) -> 'Query':
         return self
 
