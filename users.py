4c2cac6425240d8a0cab9adf3ab9f3f496e67837
osmukkaHY <oskari.lappalainen.hy@gmail.com>
2025-11-17 20:01:22 +0200

    Changed users.py to use functions instead of a class

--- users.py
+++ users.py
@@ -1,52 +1,48 @@
 from werkzeug.security import check_password_hash
 
-from db import DB
 from query import query
 
 
-class Users:
-    def __init__(self) -> None:
-        self.__db = DB()
 
     
-    def exists(self, username: str) -> bool:
-        return True if query().select('1')             \
-                              .from_('Users')          \
-                              .where('username = ?')   \
-                              .execute(username)       \
-                              .fetchone()              \
-        else False
+def user_exists(username: str) -> bool:
+    return True if query().select('1')             \
+                          .from_('Users')          \
+                          .where('username = ?')   \
+                          .execute(username)       \
+                          .fetchone()              \
+    else False
+
+
+def validate_user(username: str, password: str) -> bool:
+    if not user_exists(username):
+        return False
+    password_hash = query().select('username, password_hash')  \
+                           .from_('Users')                     \
+                           .where('username = ?')              \
+                           .execute(username)                  \
+                            .fetchone()[1]
+
+    return check_password_hash(password_hash, password)
+
+
+def create_user(username: str, password_hash: str) -> bool:
+    if user_exists(username):
+        return False
+    return True if query().insert_into('Users (username, password_hash)')  \
+                          .values('(?, ?)')                                \
+                          .execute(username, password_hash)                \
+                          .lastrowid                                       \
+    else False
+
+
+def delete(username: str) -> bool:
+    if not user_exists(username):
+        return False
     
-
-    def validate(self, username: str, password: str) -> bool:
-        if not self.exists(username):
-            return False
-        password_hash = query().select('username, password_hash')  \
-                               .from_('Users')                     \
-                               .where('username = ?')              \
-                               .execute(username)                  \
-                               .fetchone()[1]
-
-        return check_password_hash(password_hash, password)
-
-
-    def add(self, username: str, password_hash: str) -> bool:
-        if self.exists(username):
-            return False
-        return True if query().insert_into('Users (username, password_hash)')  \
-                              .values('(?, ?)')                                \
-                              .execute(username, password_hash)                \
-                              .lastrowid                                       \
-        else False
-
-
-    def delete(self, username: str) -> bool:
-        if not self.exists(username):
-            return False
-        
-        return True if query().delete('')              \
-                              .from_('Users')          \
-                              .where('username = ?')   \
-                              .execute(username)       \
-                              .rowcount                \
-        else False
+    return True if query().delete('')              \
+                          .from_('Users')          \
+                          .where('username = ?')   \
+                          .execute(username)       \
+                          .rowcount                \
+    else False
