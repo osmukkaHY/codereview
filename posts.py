c3cc65e3e20000f4abe5d0dacaa641e926987195
osmukkaHY <oskari.lappalainen.hy@gmail.com>
2025-12-03 16:54:04 +0200

    added username as a post property

--- posts.py
+++ posts.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 
 from db import DB
 from query import query
+import user
 
 
 @dataclass
@@ -13,6 +14,7 @@ class Post:
     title:      str
     context:    str
     content:    str
+    username:   str
 
 
 class Posts:
@@ -26,9 +28,10 @@ class Posts:
             title: str,
             context: str,
             content: str) -> None:
-        query().insert_into('Posts (poster_id, lang, title, context, content)')  \
-               .values('(?, ?, ?, ?, ?)')                                     \
-               .execute(user_id, language, title, context, content)
+        username = user.uname(user_id)
+        query().insert_into('Posts (poster_id, lang, title, context, content, poster_username)')  \
+               .values('(?, ?, ?, ?, ?, ?)')                                     \
+               .execute(user_id, language, title, context, content, username)
 
 
     def update(self,
@@ -44,7 +47,7 @@ class Posts:
 
 
     def by_id(self, id: int) -> Post | None:
-        post = query().select('id, ts, poster_id, lang, title, context, content')  \
+        post = query().select('id, ts, poster_id, lang, title, context, content, poster_username')  \
                              .from_('posts')                                        \
                              .where('id = ?')                                       \
                              .execute(id)                                           \
@@ -52,11 +55,11 @@ class Posts:
         if not post:
             return None
 
-        return Post(post[0], post[1], post[2], post[3], post[4], post[5], post[6])
+        return Post(post[0], post[1], post[2], post[3], post[4], post[5], post[6], post[7])
 
 
     def n_recent(self, n: int):
-        post_tuples = query().select('id, ts, poster_id, lang, title, context, content') \
+        post_tuples = query().select('id, ts, poster_id, lang, title, context, content, poster_username') \
                        .from_('Posts')                                      \
                        .order_by('ts')                                      \
                        .limit('?')                                          \
@@ -68,11 +71,12 @@ class Posts:
                      post[3],
                      post[4],
                      post[5],
-                     post[6]) for post in post_tuples]
+                     post[6],
+                     post[7]) for post in post_tuples]
 
 
     def by_user(self, user_id: str) -> list[Post]:
-        post_tuples = query().select('id, ts, poster_id, lang, title, context, content')  \
+        post_tuples = query().select('id, ts, poster_id, lang, title, context, content, poster_username')  \
                              .from_('Posts')                                        \
                              .where('poster_id = ?')                                \
                              .order_by('ts')                                        \
@@ -84,11 +88,12 @@ class Posts:
                      post[3],
                      post[4],
                      post[5],
-                     post[6]) for post in post_tuples]
+                     post[6],
+                     post[7]) for post in post_tuples]
 
 
     def search(self, keyword: str, language: str) -> Post | None:
-        post_tuples = query().select('id, ts, poster_id, lang, title, context, content')  \
+        post_tuples = query().select('id, ts, poster_id, lang, title, context, content, poster_username')  \
                              .from_('Posts')                                        \
                              .where('content LIKE ?' + ' AND lang LIKE ?')             \
                              .execute('%'+keyword+'%', language)                              \
@@ -102,4 +107,5 @@ class Posts:
                      post[3],
                      post[4],
                      post[5],
-                     post[6]) for post in post_tuples]
+                     post[6],
+                     post[7]) for post in post_tuples]
