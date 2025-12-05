dd3d75f06ea7ffeb60ba2bca67f11f9a8188462f
osmukkaHY <oskari.lappalainen.hy@gmail.com>
2025-12-05 14:46:26 +0200

    Rewrote database

--- db.py
+++ db.py
@@ -1,46 +1,17 @@
 import sqlite3
-import os
 
-from config import  db_file, db_schema_file
-
-
-class DB:
-    def __init__(self) -> None:
-        self.__db_file = db_file
-        if not os.path.isfile(db_file):
-            self.__init_db()
+from config import db_file
 
 
-    def __init_db(self) -> None:
-        # Create tables for the new database.
-        with open(db_schema_file, 'r') as file:
-            script = file.read()
-        try:
-            with sqlite3.connect(self.__db_file) as conn:
-                conn.executescript(script)
 
-        except sqlite3.Error:
-            print('DB.__init_db: '
-                  'An error occurred while initializing a database.')
+def execute(sql: str, *args) -> None:
+    with sqlite3.Connection(db_file) as conn:
+        conn.execute("PRAGMA foreign_keys = ON")
+        return conn.execute(sql, args).rowcount
 
 
-    def fetch(self, query: str, *args) -> list[tuple] | None:
-        try:
-            with sqlite3.connect(self.__db_file) as conn:
-                result = conn.execute(query, args).fetchall()
-            return result
-        except sqlite3.OperationalError:
-            print('DB.execute: '
-                 f'Couldn\'t execute query "{query}" with arguments {args}')
-            return None
-        
-
-    def insert(self, query: str, *args) -> bool | None:
-        try:
-            with sqlite3.connect(self.__db_file) as conn:
-                conn.execute(query, args)
-            return True
-        except:
-            print('DB.execute: '
-                 f'Couldn\'t execute query "{query}" with arguments {args}')
-            return None
+def query(sql: str, *args) -> list[sqlite3.Row]:
+    with sqlite3.Connection(db_file) as conn:
+        conn.execute("PRAGMA foreign_keys = ON")
+        conn.row_factory = sqlite3.Row
+        return conn.execute(sql, args).fetchall()
